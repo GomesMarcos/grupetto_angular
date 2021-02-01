0819b722e127d525739293886336ba5c1e93c933
GomesMarcos <gomes.marcosjf@gmail.com>
2021-02-01 16:53:13 -0300

    generate radem intervals and repeat them

--- frontend/src/app/keyboard/keyboard.component.ts
+++ frontend/src/app/keyboard/keyboard.component.ts
@@ -8,21 +8,25 @@ import * as octaves from './octaves.js'
   templateUrl: './keyboard.component.html',
   styleUrls: ['./keyboard.component.scss'],
 })
-export class KeyboardComponent implements OnInit {
+export class KeyboardComponent {
   // ATTRIBUTES
   keys: HTMLElement
+  duration: Number
   white_notes: Object[]
   synth = new Tone.PolySynth().toDestination()
   note: Frequency
-  interval: Number
+  notes: HTMLCollection
+  interval: any
   keyOctaves: Array<Array<String>>
 
   constructor() {
     this.keyOctaves = octaves.default
+    this.duration = 500
   }
 
-  ngOnInit(): void {
-    this.interval = this.setInterval()
+  ngAfterViewInit(): void {
+    this.notes = document.getElementsByClassName('keyboard')
+    this.interval = this.setInterval(this.notes)
   }
 
   onMouseDown(event): void {
@@ -46,13 +50,41 @@ export class KeyboardComponent implements OnInit {
 
   playNote(key): void {
     this.note = key.id
-    this.synth.triggerAttackRelease(this.note, '16n')
+    this.synth.triggerAttackRelease(this.note, (this.duration / 1000).toString())
   }
 
-  setInterval() {
-    const first_key = Math.floor(Math.random() * 12)
-    const second_key = Math.floor(Math.random() * 12)
+  setInterval(notes) {
+    const first_key = Math.floor(Math.random() * notes.length) - 1
+    const second_key = Math.floor(Math.random() * notes.length) - 1
 
-    return second_key - first_key
+    if (this.validateInterval(first_key, second_key)) {
+      this.activePlayedNote(notes[first_key], this.duration)
+      this.activePlayedNote(notes[second_key], this.duration)
+
+      this.interval = [first_key, second_key]
+
+      return [first_key, second_key]
+    }
+    this.setInterval(notes)
+  }
+
+  replayInterval(interval) {
+    this.activePlayedNote(this.notes[interval[0]], this.duration)
+    this.activePlayedNote(this.notes[interval[1]], this.duration)
+  }
+
+  activePlayedNote(note, noteDuration) {
+    note.classList.add('active')
+    this.playNote(note)
+    setTimeout(() => {
+      note.classList.remove('active')
+    }, noteDuration)
+  }
+
+  validateInterval(first_key, second_key): boolean {
+    if (second_key - first_key < 0) {
+      return false
+    }
+    return true
   }
 }
